b1bfd5d8551330b957c6226dd0f59d1d785859f6
WAGNER BORGES <emaildowagnerborges@gmail.com>
2017-10-04 08:19:31 -0300

    comite inicial do projeto

---
+++ src/main/java/com/seed/zaqueu/api/ZaqueuApiApplication.java
@@ -0,0 +1,12 @@
+package com.seed.zaqueu.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaqueuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaqueuApiApplication.class, args);
+	}
+}
